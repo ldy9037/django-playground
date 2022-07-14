547b5789ef497bcb19095dcb61f85f8df6f16c1d
emadam <ldy9037@naver.com>
2022-07-14 09:01:55 +0900

    test: 직렬화 test case 작성

---
+++ tutorial/snippets/tests/test_seirializer.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+class SnippetSerializerTests(TestCase):
+    def setUp(self):
+        self.snippet = Snippet(code='foo = "bar"\n')
+        self.snippet.save()
+        
+        self.snippet = Snippet(code='print("hello, world")\n')
+        self.snippet.save()
+
+    def test_serializer(self):
+        serializer = SnippetSerializer(self.snippet)
+        self.assertEqual(serializer.data, {'id': 2, 'title': '', 'code': 'print(\"hello, world\")\n', 'linenos': False, 'language': 'python', 'style': 'friendly'})
