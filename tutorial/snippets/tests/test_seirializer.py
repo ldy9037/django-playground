251d12859eeb4b599722154b95792baf3bd82e82
emadam <ldy9037@naver.com>
2022-07-14 09:17:30 +0900

    test: 역직렬화에 대한 test 로직 추가

--- tutorial/snippets/tests/test_seirializer.py
+++ tutorial/snippets/tests/test_seirializer.py
@@ -4,6 +4,8 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from collections import OrderedDict
+import io
 
 class SnippetSerializerTests(TestCase):
     def setUp(self):
@@ -19,3 +21,14 @@ class SnippetSerializerTests(TestCase):
 
         content = JSONRenderer().render(serializer.data)
         self.assertEqual(content, b'{"id":2,"title":"","code":"print(\\"hello, world\\")\\n","linenos":false,"language":"python","style":"friendly"}')
+
+        stream = io.BytesIO(content)
+        data = JSONParser().parse(stream)
+        serializer = SnippetSerializer(data=data)
+        
+        self.assertTrue(serializer.is_valid())
+        self.assertEqual(serializer.validated_data, OrderedDict([('title', ''), ('code', 'print("hello, world")'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]))
+        serializer.save()
+
+        serializer = SnippetSerializer(Snippet.objects.all(), many=True)
+        self.assertEqual(serializer.data, [OrderedDict([('id', 1), ('title', ''), ('code', 'foo = "bar"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('id', 2), ('title', ''), ('code', 'print("hello, world")\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('id', 3), ('title', ''), ('code', 'print("hello, world")'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])])
