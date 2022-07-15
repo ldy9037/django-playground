7d68713fcda8d5f6c30912ab25ecaf58a03f7ff0
emadam <ldy9037@naver.com>
2022-07-15 09:00:01 +0900

    test: snippet 상세/수정/삭제에 대한 test case 작성

--- tutorial/snippets/tests/test_url.py
+++ tutorial/snippets/tests/test_url.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 import io
 
+from snippets.serializers import SnippetSerializer
+
 class SnippetURLTests(APITestCase):
     
     def test_snippet_list(self):
@@ -25,3 +27,35 @@ class SnippetURLTests(APITestCase):
         data = JSONParser().parse(stream)
         self.assertEqual(len(data), 1)
         
+    def test_snippet_detail(self):
+        snippet = Snippet.objects.create(code = "test")
+        snippet.save()
+
+        url = reverse('snippet_detail', kwargs={'pk': snippet.id})
+        response = self.client.get(url, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        
+        stream = io.BytesIO(response.content)
+        data = JSONParser().parse(stream)
+
+        self.assertEqual(data['id'], snippet.id)
+        self.assertEqual(data['title'], snippet.title)
+        self.assertEqual(data['code'], snippet.code)
+        self.assertEqual(data['linenos'], snippet.linenos)
+        self.assertEqual(data['language'], snippet.language)
+        self.assertEqual(data['style'], snippet.style)
+        
+        data['title'] = "title1"
+
+        response = self.client.put(url, data, format='json')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+        stream = io.BytesIO(response.content)
+        data = JSONParser().parse(stream)
+
+        self.assertEqual(data['id'], snippet.id)
+        self.assertNotEqual(data['title'], snippet.title)
+
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Snippet.objects.count(), 0)
