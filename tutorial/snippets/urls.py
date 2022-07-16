09c025b9a62c0bc61605e682268d9bc7ba9cd68d
emadam <ldy9037@naver.com>
2022-07-16 21:16:40 +0900

    feat: user view를 /users에 매핑

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,6 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view(), name='snippet_list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
+
+    path('users/', views.UserList.as_view(), name='user_list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
