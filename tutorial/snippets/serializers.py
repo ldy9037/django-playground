d6b98b1ef3d8d22d22f1c1f8cf257b74a665e5b0
emadam <ldy9037@naver.com>
2022-07-16 18:14:21 +0900

    feat: Snippet serializer에 owner field 추가

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 class UserSerializer(serializers.ModelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
