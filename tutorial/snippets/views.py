9de17ae9bf0b7da99e68d8db157daee1ec492f3d
emadam <ldy9037@naver.com>
2022-07-17 07:46:22 +0900

    feat: snippet detail에 본인 snippet만 수정/삭제 가능하도록 권한 변경

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -15,8 +16,8 @@ class SnippetList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
