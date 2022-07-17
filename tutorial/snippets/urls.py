99e84fee45cbea6e446925f495902dc0ecfad823
emadam <ldy9037@naver.com>
2022-07-17 20:54:49 +0900

    feat: 루트 경로에 대한 view 작성 - 다른 엔드포인트에 대한 단일 진입 지점생성 - 루트 엔드포인트로 요청 시 다른 엔트포인트 정보 제공

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,8 @@ from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    path('', views.api_root, name='api_root'),
+
     path('snippets/', views.SnippetList.as_view(), name='snippet_list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet_detail'),
 
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,6 +2,11 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
@@ -28,3 +33,10 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user_list', request=request, format=format),
+        'snippets': reverse('snippet_list', request=request, format=format)
+    })
